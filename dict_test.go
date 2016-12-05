b049991a46a2f619344bd6e915745703864d0134
Felix Hanley <felix@userspace.com.au>
2016-12-05 15:16:58 +0700

    Clean up source structure and update vendor versions

---
+++ dict_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+type mockResponseWriter struct{}
+
+func (m *mockResponseWriter) Header() (h http.Header) {
+	return http.Header{}
+}
+
+func (m *mockResponseWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func (m *mockResponseWriter) WriteString(s string) (n int, err error) {
+	return len(s), nil
+}
+
+func (m *mockResponseWriter) WriteHeader(int) {}
+
+func testGetDictClient(t *testing.T) {
+}
